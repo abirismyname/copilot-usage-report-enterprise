264477b6e5b1eb80b031bc7d3d5334dccfa0da3e
Abir Majumdar <abirismyname@github.com>
2024-06-25 14:21:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# Copilot Usage Report - Enterprise Level
+Export the Copilot seat assignments for an Enterprise that are currently being billed
+
+> Note: This Action uses the [List all Copilot seat assignments for an Enterprise](https://docs.github.com/en/rest/copilot/copilot-user-management?apiVersion=2022-11-28#list-all-copilot-seat-assignments-for-an-enterprise), which is in public Beta and subject to change
+
+## PAT Token
+Create a Classic personal access tokens with `read:enterprise` scope.
+
+Pass this token as an input to the action - GITHUB_TOKEN
+
+
+## Action in workflow
+
+Include the copilot-usage-report action in your workflow. 
+
+**1. Sample workflow 1: Export to CSV**
+
+```
+    name: Copilot Usage Report export to CSV 
+
+    on:
+      workflow_dispatch:
+
+    jobs:
+      first-job:
+        runs-on: ubuntu-latest
+        
+        steps:
+        - name: Copilot usage
+            uses: copilot-usage-report-enterprise@main
+            with:        
+            GITHUB_TOKEN: ${{secrets.ORG_TOKEN}}
+            ent_name: 'octodemo'
+            file_path: data/Copilot-Usage-Report.csv
+        
+        - name: Upload Copilot Usage Report
+            uses: actions/upload-artifact@v3
+            with:
+            name: Copilot Usage Report
+            path: data/Copilot-Usage-Report.csv      
+```
+
+**1. Sample workflow 2: Export to JSON**
+
+```
+    name: Copilot Usage Report export to JSON
+
+    on:
+      workflow_dispatch:
+
+    jobs:
+      first-job:
+        runs-on: ubuntu-latest
+        
+        steps:
+        - name: Copilot usage
+            uses: copilot-usage-report-enterprise@main
+            with:        
+            GITHUB_TOKEN: ${{secrets.ORG_TOKEN}}
+            ent_name: 'octodemo'
+            file_path: data/Copilot-Usage-Report.json
+        
+        - name: Upload Copilot Usage Report
+            uses: actions/upload-artifact@v3
+            with:
+            name: Copilot Usage Report
+            path: data/Copilot-Usage-Report.json      
+```
+
+## Parameters
+
+| Name                           | Required  | Description                                                           |
+|--------------------------------|------------|----------------------------------------------------------------------|
+| GITHUB_TOKEN                 | Yes | PAT Token for access    |
+| ent_name                       | Yes | GitHub Organization Name                                      |
+| file_path                       | Yes | CSV or JSON file path                                   |
+
+## Exported Fields
+Following fields are included in the Copilot Usage Report
+- User
+- Created At
+- Updated At
+- Last Acivity At
+- Last Acivity Editor
+- Pending Cancellation Date
+- Team
+
+## Report
+Copilot usage report is added as a build artifact in the workflow. You can download the report from the workflow run page.
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](./LICENSE)
---
+++ app/index.js
@@ -0,0 +1,142 @@
+// libs for github & graphql
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { parse } = require('json2csv');
+
+// libs for csv file creation
+const { dirname } = require("path");
+const makeDir = require("make-dir");
+const fs = require('fs');
+
+// get the octokit handle 
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+// inputs defined in action metadata file
+const ent_name = core.getInput('ent_name');
+const file_path = core.getInput('file_path');
+
+let totalSeats = 0;
+
+// Our CSV output fields
+const fields = [
+    {
+        label: 'User',
+        value: 'assignee.login'
+    },
+    {
+        label: 'Created At',
+        value: 'created_at'
+    },
+    {
+        label: 'Updated At',
+        value: 'updated_at'
+    },
+    {
+        label: 'Last Acivity At',
+        value: 'last_activity_at'
+    },
+    {
+        label: 'Last Acivity Editor',
+        value: 'last_activity_editor'
+    },
+    {
+        label: 'Pending Cancellation Date',
+        value: 'pending_cancellation_date'
+    },
+    {
+        label: 'Team',
+        value: 'assigning_team.name'
+    }
+];
+
+// Copilot User Management API call
+async function getUsage(ent, pageNo) {
+    try {
+
+        return await octokit.request('GET /enterprise/{ent}/copilot/billing/seats', {
+            ent: ent_name,
+            page: pageNo,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// Extract Copilot usage data with a pagination of 50 records per page
+async function run(ent_name, file_path) {
+
+    let addTitleRow = true;
+    let pageNo = 1;
+    let remainingRecs = 0;
+
+    try {
+        await makeDir(dirname(file_path));
+        //delete the file, if exists
+        if (fs.existsSync(file_path)) {
+            fs.unlinkSync(file_path);
+        }
+        do {
+            // invoke the graphql query execution
+            await getUsage(ent_name, pageNo).then(usageResult => {
+                let seatsData = usageResult.data.seats;
+
+                if (addTitleRow) {
+                    totalSeats = usageResult.data.total_seats;
+                    console.log('Seat Count ' + totalSeats);
+                    remainingRecs = totalSeats;
+                }
+
+                // check whether the file extension is csv or not
+                if (file_path.endsWith('.csv')) {
+
+                    // ALERT! - create our updated opts
+                    const opts = { fields, "header": addTitleRow };
+
+                    // append to the existing file (or create and append if needed)
+                    fs.appendFileSync(file_path, `${parse(seatsData, opts)}\n`);
+                } else {
+                    // Export to JSON file
+                    //check the file exists or not 
+                    if (!fs.existsSync(file_path)) {
+                        // The file doesn't exist, create a new one with an empty JSON object
+                        fs.writeFileSync(file_path, JSON.stringify([], null, 2));
+                    }
+
+                    //check the file is empty or not
+                    let data = fs.readFileSync(file_path, 'utf8'); // read the file
+
+                    // file contains only [] indicating a blank file
+                    // append the entire data to the file
+                    if (data.trim() === '[]') {
+                        console.log("The JSON data array is empty.");
+                        fs.writeFileSync(file_path, JSON.stringify(seatsData, null, 2));
+                    } else {
+                        //TODO: find the delta and append to existung file
+                        let jsonData = JSON.parse(data); // parse the JSON data into a JavaScript array
+                        jsonData = jsonData.concat(seatsData);
+                        fs.writeFileSync(file_path, JSON.stringify(jsonData, null, 2));
+                    }
+                }
+                // pagination to get next page data
+                remainingRecs = remainingRecs - seatsData.length;
+                console.log('Remaining Records ' + remainingRecs);
+                if (remainingRecs > 0) {
+                    pageNo = pageNo + 1;
+                    addTitleRow = false;
+                }
+            });
+        } while (remainingRecs > 0);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+console.log(`preamble: ent name: ${ent_name} `);
+
+// run the action code
+run(ent_name, file_path);
